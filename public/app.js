c69e2bae231fd71f149dbb4605c36e3e7bf61cb2
flowermariya <flowermariya811@gmail.com>
2020-11-12 15:08:56 -0700

    hi

---
+++ public/app.js
@@ -0,0 +1,30 @@
+console.log(hi)
+
+/*const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+const userDetails = document.getElementById('userDetails');*/
+const firebase = require('firebase');
+
+/*var app = auth.app
+app.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
+
+<section id="whenSignedOut">
+  <button id="signInBtn">Sign in with Google</button>
+</section>
+
+<section id="whenSignedIn" hidden="true">
+  <div id="userDetails"></div>
+  <button id="signOutBtn">Sign Out</button>
+</section>*/
